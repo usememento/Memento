34e2ba867bd7e371af325b19432aaa641f44f9eb
wgh19 <nyne19710@proton.me>
2024-06-30 23:00:27 +0800

    update

---
+++ frontend/lib/foundation/widget_utils.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/widgets.dart';
+import 'package:frontend/foundation/page_route.dart';
+
+extension WidgetExtension on Widget{
+  Widget padding(EdgeInsetsGeometry padding){
+    return Padding(padding: padding, child: this);
+  }
+
+  Widget paddingLeft(double padding){
+    return Padding(padding: EdgeInsets.only(left: padding), child: this);
+  }
+
+  Widget paddingRight(double padding){
+    return Padding(padding: EdgeInsets.only(right: padding), child: this);
+  }
+
+  Widget paddingTop(double padding){
+    return Padding(padding: EdgeInsets.only(top: padding), child: this);
+  }
+
+  Widget paddingBottom(double padding){
+    return Padding(padding: EdgeInsets.only(bottom: padding), child: this);
+  }
+
+  Widget paddingVertical(double padding){
+    return Padding(padding: EdgeInsets.symmetric(vertical: padding), child: this);
+  }
+
+  Widget paddingHorizontal(double padding){
+    return Padding(padding: EdgeInsets.symmetric(horizontal: padding), child: this);
+  }
+
+  Widget paddingAll(double padding){
+    return Padding(padding: EdgeInsets.all(padding), child: this);
+  }
+
+  Widget toCenter(){
+    return Center(child: this);
+  }
+
+  Widget toAlign(AlignmentGeometry alignment){
+    return Align(alignment: alignment, child: this);
+  }
+
+  Widget sliverPadding(EdgeInsetsGeometry padding){
+    return SliverPadding(padding: padding, sliver: this);
+  }
+
+  Widget sliverPaddingAll(double padding){
+    return SliverPadding(padding: EdgeInsets.all(padding), sliver: this);
+  }
+
+  Widget sliverPaddingVertical(double padding){
+    return SliverPadding(padding: EdgeInsets.symmetric(vertical: padding), sliver: this);
+  }
+
+  Widget sliverPaddingHorizontal(double padding){
+    return SliverPadding(padding: EdgeInsets.symmetric(horizontal: padding), sliver: this);
+  }
+
+  Widget fixWidth(double width){
+    return SizedBox(width: width, child: this);
+  }
+
+  Widget fixHeight(double height){
+    return SizedBox(height: height, child: this);
+  }
+}
+
+extension ContextExt on BuildContext {
+  void pop<T>([T? result]) {
+    Navigator.of(this).pop(result);
+  }
+
+  Future<T?> to<T>(String path) {
+    return Navigator.of(this).pushNamed(path);
+  }
+
+  Size get size => MediaQuery.of(this).size;
+
+  EdgeInsets get padding => MediaQuery.of(this).padding;
+
+  EdgeInsets get viewInsets => MediaQuery.of(this).viewInsets;
+}
