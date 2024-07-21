7fd212f542d3410a5e0832993435958ea58f7bd7
wgh19 <nyne19710@proton.me>
2024-07-21 14:42:58 +0800

    fix build.py

--- build.py
+++ build.py
@@ -6,11 +6,11 @@ if os.path.exists("build"):
     shutil.rmtree("build")
 os.mkdir("build")
 
-subprocess.run(["go", "build", "-o", "build/", "main.go"], shell=True)
+subprocess.run(["go", "build", "-o", "build/", "main.go"])
 
 shutil.copytree("./assets", "./build/assets")
 
 os.chdir("./frontend")
-subprocess.run(["flutter", "build", "web", "--web-renderer", "html", "--release"], shell=True)
+subprocess.run(["flutter", "build", "web", "--web-renderer", "html", "--release"])
 os.chdir("..")
 shutil.copytree("./frontend/build/web", "./build/frontend/build/web")
