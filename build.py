515455928d41fef48d207f39a43c14c2b79db0b1
wgh19 <nyne19710@proton.me>
2024-07-25 13:01:33 +0800

    fix build script

--- build.py
+++ build.py
@@ -11,6 +11,9 @@ subprocess.run(["go", "build", "-o", "build/", "main.go"])
 shutil.copytree("./assets", "./build/assets")
 
 os.chdir("./frontend")
-subprocess.run(["flutter", "build", "web", "--web-renderer", "html", "--release"])
+if os.name == "nt":
+    subprocess.run(["flutter.bat", "build", "web", "--web-renderer", "html", "--release"])
+else:
+    subprocess.run(["flutter", "build", "web", "--web-renderer", "html", "--release"])
 os.chdir("..")
 shutil.copytree("./frontend/build/web", "./build/frontend/build/web")
