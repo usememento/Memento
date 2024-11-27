25c52f5b161a83c691d5719e3def0fdd8a811dcf
nyne <me@nyne.dev>
2024-11-27 11:10:43 +0800

    token

--- web/src/network/network.ts
+++ web/src/network/network.ts
@@ -1,35 +1,65 @@
 import app from "../app.ts";
 import {Post} from "./model.ts";
+import axios from 'axios';
 
 export const network = {
-    getPosts: async (username: string, page: number) => {
-        const res = await fetch(`${app.server}/api/post/userPosts?username=${username}&page=${page}`, {
-            headers: {
-                "Authorization": app.token ?? ""
-            }
+    isRefreshing: false,
+    init: () => {
+        axios.interceptors.request.use((config) => {
+            if(app.token)
+                config.headers.Authorization = app.token;
+            return config;
         });
-        if(res.ok) {
-            const json = await res.json();
+        axios.interceptors.response.use(async (res) => {
+            if(res.status === 401 && !network.isRefreshing) {
+                try {
+                    network.isRefreshing = true;
+                    await network.refreshToken();
+                    res = await axios.request(res.config);
+                    return res;
+                }
+                catch (e) {
+                    console.error("Failed to refresh token: ", e);
+                    return res;
+                }
+                finally {
+                    network.isRefreshing = false;
+                }
+            }
+            return res;
+        });
+    },
+    refreshToken: async () => {
+        const res = await axios.postForm(`${app.server}/api/auth/refresh`, {
+            refreshToken: app.refreshToken,
+        });
+        app.token = res.data.token;
+    },
+    getPosts: async (username: string, page: number) => {
+        const res = await axios.get(`${app.server}/api/post/userPosts?username=${username}&page=${page}`);
+        if(res.status === 200) {
+            const json = await res.data;
             return [json.posts as Post[], json.maxPage as number];
         } else {
-            const json = await res.json();
+            const json = res.data;
             throw new Error(json.message);
         }
     },
     createPost: async (content: string, isPublic: boolean) => {
-        const res = await fetch(`${app.server}/api/post/create`, {
+        const res = await axios.postForm(`${app.server}/api/post/create`, {
             method: "POST",
-            body: "content=" + encodeURIComponent(content) + "&permission=" + (isPublic ? "public" : "private"),
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Authorization": app.token ?? ""
-            }
+            body: {
+                content: content,
+                permission: isPublic ? "public" : "private",
+            },
         });
-        if(res.ok) {
+        if(res.status === 200) {
             return;
         } else {
-            const json = await res.json();
+            const json = res.data;
             throw new Error(json.message);
         }
     }
 }
+
+network.init();
